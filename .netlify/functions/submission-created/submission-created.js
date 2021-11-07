2bb5b6930b7a5c1525a0690472a0953195d1149d
Matthew Bloomfield <mbloomfield@uschamber.com>
2021-11-07 15:40:29 -0700

    changing to a post

--- .netlify/functions/submission-created/submission-created.js
+++ .netlify/functions/submission-created/submission-created.js
@@ -4,7 +4,9 @@ const API_ENDPOINT = 'https://api.netlify.com/build_hooks/61883da143db9fcf939555
 
 exports.handler = async (event, context) => {
   try {
-    const response = await fetch(API_ENDPOINT);
+    const response = await fetch(API_ENDPOINT, {
+      method: "POST",
+    });
     const data = await response.json();
     return { statusCode: 200, body: JSON.stringify({ data }) };
   } catch (error) {
