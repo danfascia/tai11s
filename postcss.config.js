5f723f2eb6698df6ea07cafb63b79788675e6927
Matthew Bloomfield <mbloomfield@uschamber.com>
2021-11-23 11:01:12 -0700

    Changing the way CSS is compiled

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,10 @@
 const tailwindcss = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
 
 const plugins = [
   autoprefixer,
-  tailwindcss('./tailwind.config.js'),
-  cssnano({ preset: 'default', }),
+  tailwindcss(),
 ]
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    purgecss({
-      content: ['./dist/**/*.html', './dist/**/**/*.html'],
-      extractors: [
-        {
-          extractor: class TailwindExtractor {
-            static extract(content) {
-              return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-            }
-          },
-          extensions: ['css', 'html', 'vue'],
-        },
-      ]
-    })
-  )
-}
 
 module.exports = {
   plugins
